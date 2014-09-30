8403a50eb3ee627351a41eca85758281539a393a
karansahu <karansahu1991@gmail.com>
2014-09-30 19:09:26 -0400

    Initial commmit

---
+++ Game Design Mini Game 2/Assets/Player1Movement.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1Movement : MonoBehaviour 
+{
+	CharacterController P1ctrl;
+	private GameObject P1;
+
+	CharacterController P2ctrl;
+	private GameObject P2;
+
+	private float timerOffset;
+	public float firstDelay;
+	private float origTime;
+
+	private bool isMoving = false;
+
+	void Start () 
+	{
+
+		P1 = GameObject.FindWithTag ("Player1");
+		P1ctrl = P1.GetComponent<CharacterController>();
+
+		P2 = GameObject.FindWithTag ("Player2");
+		P2ctrl = P2.GetComponent<CharacterController>();
+	}
+	
+
+	void Update () 
+	{
+		if(isMoving)
+		{
+			playerMove();
+		}
+		else if(!isMoving)
+		{
+			TimerReset();
+		}
+
+		if(
+	}
+
+	void playerMove()
+	{
+		if(DestNum < Destinations.Length)
+		{
+			P1.transform.position = Vector3.Lerp (P1.transform.position, Destinations[DestNum].position,(Time.time - timerOffset) * speed);
+			delayCheck = true;
+		}
+	}	
+
+	void TimerReset()
+	{
+		timerOffset = Time.time;
+	}
+}
